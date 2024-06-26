d8f77a1a639e8ed40ec119f7abb0459e863dc2a3
gyasi.eth <104705433+barigyasi@users.noreply.github.com>
2024-06-26 16:59:52 -0500

    new branch to test v5 connectButton

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,10 +6,69 @@ import Image from "next/image";
 import Link from "next/link";
 import { ThemeToggle } from "./ThemeToggle";
 import { useTheme } from "next-themes";
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectButton, useActiveAccount } from "thirdweb/react";
 import MobileNavigation from "./MobileNavigation";
+import { createWallet } from "thirdweb/wallets";
+import { createThirdwebClient, defineChain, getContract } from "thirdweb";
+import { baseSepolia } from "thirdweb/chains";
+import { useEffect, useState } from "react";
+import { resolveName } from "thirdweb/extensions/ens";
 
-export function Header() {
+const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+const secretKey = process.env.NEXT_PUBLIC_SECRET_KEY;
+const NFT_COLLECTION_ADDRESS = "0x92F2666443EBFa7129f39c9E43758B33CD5D73F8";
+
+interface HeaderProps {
+  client: any;
+  wallets: any[];
+  chain: any;
+}
+
+const wallets = [
+  createWallet("com.coinbase.wallet"),
+  createWallet("io.metamask"),
+];
+
+const Header = () => {
+  const account = useActiveAccount();
+  const [wallet, setWallet] = useState<string | null>(null);
+  const [creatorName, setCreatorName] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (account?.address) {
+      setWallet(account.address);
+      fetchEnsName(account.address);
+    }
+  }, [account]);
+
+  const client = createThirdwebClient({
+    secretKey: secretKey as string, // ensure that secretKey is not undefined
+  });
+
+  const chain = defineChain(84532);
+  const contract = getContract({
+    client,
+    chain,
+    address: NFT_COLLECTION_ADDRESS,
+  });
+
+  const fetchEnsName = async (address: string) => {
+    try {
+      const ensName = await resolveName({
+        client,
+        address: address,
+      });
+      if (ensName) {
+        setCreatorName(ensName);
+      } else {
+        setCreatorName(address.slice(0, 3) + '...' + address.slice(-3));
+      }
+    } catch (error) {
+      console.error("Error resolving ENS name:", error);
+      setCreatorName(address.slice(0, 3) + '...' + address.slice(-3));
+    }
+  };
+
   const { theme } = useTheme();
 
   return (
@@ -28,14 +87,37 @@ export function Header() {
               </div>
             </Link>
           </div>
-          <div className="hidden md:flex md:gap-x-6 ">
+          <div className="hidden md:flex md:gap-x-6">
             <NavLink href="https://x.com/PublicGoodsClub" target="_blank">Community</NavLink>
             <NavLink href="https://mirror.xyz/bigtrav.eth/6hD4LTjGWC8TXef4DGIxbdVSibreKLTWila-wOku0DM" target="_blank">Learn</NavLink>
             <NavLink href="/"> My NFTs</NavLink>
           </div>
           <div className="flex items-center gap-x-5 md:gap-x-8">
             <div className="hidden md:block">
-              <ConnectWallet  switchToActiveChain={true} />
+              <ConnectButton 
+                client={client}
+                wallets={wallets}
+                chain={chain}
+                theme={"dark"}
+                appMetadata={{
+                  name: "Avatar",
+                  url: "https://example.com",
+                }}
+                connectButton={{ label: "Log In or Sign Up" }}
+                connectModal={{
+                  size: "wide",
+                  title: "Choose Method",
+                  welcomeScreen: {
+                    title: "PublicGoodsClub",
+                    img: {
+                      src: "https://media.discordapp.net/attachments/1244435318006874163/1248808384753434634/PGC_Flower_Logo.png?ex=666502f0&is=6663b170&hm=88b38c7b5a86511dcb2d2e5c6d5c02ecde91dd8a880a65ada02924a5db318d87&=&format=webp&quality=lossless",
+                      width: 150,
+                      height: 150,
+                    },
+                  },
+                  showThirdwebBranding: false,
+                }}
+              />
             </div>
             <ThemeToggle />
             <div className="-mr-1 md:hidden">
@@ -46,4 +128,6 @@ export function Header() {
       </Container>
     </header>
   );
-}
+};
+
+export default Header;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider as ThirdwebProviderV5 } from "thirdweb/react"
 import { BaseSepoliaTestnet } from "@thirdweb-dev/chains";
 import { clientId } from "./const/constants";
-import { Header } from "./components/Header";
+import Header from "./components/Header"; // Ensure this import statement is correct
 import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,13 +19,14 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-background-light dark:bg-background-dark min-h-screen">
         <ThirdwebProvider activeChain={BaseSepoliaTestnet} clientId={clientId}>
-          <ThemeProvider attribute="class">
-          <Header />
-          <main className="flex-grow">
-          
-          {children}
-          </main>
-          </ThemeProvider>
+          <ThirdwebProviderV5>
+            <ThemeProvider attribute="class">
+              <Header />
+              <main className="flex-grow">
+                {children}
+              </main>
+            </ThemeProvider>
+          </ThirdwebProviderV5>
         </ThirdwebProvider>
       </body>
     </html>
